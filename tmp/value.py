d6df30cb387220a57d930b1808be82338f96db01
andrea <andrea.cappello@studenti.unitn.it>
2020-03-03 22:00:18 +0200

    ...

---
+++ tmp/value.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+datetime.now(tz=None)
+dateTimeObj = datetime.now()
+
+''' Vehicle 1 - BIKE '''
+
+nf1 = 410
+
+f= open("v1.txt","w+")
+# timeST_lon_lat
+
+for i in range(nf1):
+     f.write("{}_{}_{}".format())
+
+
+f.close()
+
+''' Vehicle 2 - CAR '''
+
+nf2 = 410
+
+f= open("v1.txt","w+")
+# timeST_lon_lat
+
+for i in range(nf2):
+     f.write("{}_{}_{}".format())
+
+
+f.close()
+
+''' Vehicle 3 - POLICE '''
+
+nf3 = 310
+
+f= open("v1.txt","w+")
+# timeST_lon_lat
+
+for i in range(nf3):
+     f.write("{}_{}_{}".format())
+
+
+f.close()
