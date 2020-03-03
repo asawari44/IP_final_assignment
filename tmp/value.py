8015eee2db4210020863a3e0d256f94a5336b5a7
andrea <andrea.cappello@studenti.unitn.it>
2020-03-03 22:24:16 +0200

    new data for us :D

--- tmp/value.py
+++ tmp/value.py
@@ -1,42 +1,42 @@
 from datetime import datetime
 datetime.now(tz=None)
 dateTimeObj = datetime.now()
+nf1 = 411
+nf2 = 411
+nf3 = 311
+
+carStep = (18000/410)/100
+bikeStep = (10250/410)/100
 
 ''' Vehicle 1 - BIKE '''
-
-nf1 = 410
-
-f= open("v1.txt","w+")
-# timeST_lon_lat
-
+f= open("v1_bike.txt","w+")
+x, y = 0, 0
 for i in range(nf1):
-     f.write("{}_{}_{}".format())
-
-
+     x = x + bikeStep
+     # timeST_x_y
+     f.write("{}_{}_{}\n".format(i, x, y))
 f.close()
 
-''' Vehicle 2 - CAR '''
 
-nf2 = 410
-
-f= open("v1.txt","w+")
+''' Vehicle 3 - POLICE '''
+x, y = 0, 0
+f= open("v3_police.txt","w+")
 # timeST_lon_lat
 
 for i in range(nf2):
-     f.write("{}_{}_{}".format())
-
-
+     x = x + bikeStep
+     # timeST_x_y
+     f.write("{}_{}_{}\n".format(i, x, y))
 f.close()
 
-''' Vehicle 3 - POLICE '''
 
-nf3 = 310
-
-f= open("v1.txt","w+")
+''' Vehicle 2 - CAR '''
+x, y = 180, 0
+f= open("v2_car.txt","w+")
 # timeST_lon_lat
 
 for i in range(nf3):
-     f.write("{}_{}_{}".format())
-
-
+     x = x - bikeStep
+     # timeST_x_y
+     f.write("{}_{}_{}\n".format(i, x, y))
 f.close()
